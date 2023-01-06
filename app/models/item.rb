2f49263edc6c62d22ad342451640d251142285e7
“Fahad <fahad.ishfaq@algorepublic.com>
2023-01-06 17:28:27 +0500

    added logo added category

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,10 @@ class Item < ApplicationRecord
 	has_many :color_items
 	has_many :colors, through: :color_items
 	accepts_nested_attributes_for :colors, reject_if: :all_blank, allow_destroy: true
+	enum category: {
+		'SYNTHETIC LEATHER': 'sythtntic_leather',
+		'OUTSOLE': 'outsole',
+		'INSOLE': 'insole',
+		'UPPER': 'upper'
+	}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_21_064018) do
+ActiveRecord::Schema.define(version: 2023_01_06_115841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_12_21_064018) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "picture"
     t.json "product_pictures"
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
