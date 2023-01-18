61a7c79cec54469f3b7f8fbec04825b725141f82
“Fahad <fahad.ishfaq@algorepublic.com>
2023-01-18 15:47:44 +0500

    minor change in email constraint.

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2023_01_18_083211) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
