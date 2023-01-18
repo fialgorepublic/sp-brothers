0d08391745c1221929a62368384253f978981424
“Fahad <fahad.ishfaq@algorepublic.com>
2023-01-18 17:01:10 +0500

    added full name in signup removed debugger

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -13,17 +13,6 @@ class AuthorizeApiRequest
   
   attr_reader :headers
   
-  # def user
-  #   token = http_auth_header
-  #   user = User.where(auth_token: token).first
-  #   if user.blank?
-  #     errors.add(:token, 'Token Missing.')
-  #   else
-  #     errors.add(:token, 'Token Expired') if user.auth_token != http_auth_header
-  #   end
-  #   user.present? ? user : nil
-  # end
-  
   def user
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
     @user || errors.add(:token, "Invalid token") && nil
